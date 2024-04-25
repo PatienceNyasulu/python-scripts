63f7c6ce748aa549bd3dc1bb71e9b772292bbed2
Czennie <83468952+PatienceNyasulu@users.noreply.github.com>
2024-04-25 06:07:26 +0200

    Update pipython.py

--- pipython.py
+++ pipython.py
@@ -21,7 +21,7 @@ client.connect("a3s1cqgu0ggloe-ats.iot.eu-central-1.amazonaws.com", 8883, 60) #T
 
 def intrusionDetector(Dummy):
     while (1):    
-	x=GPIO.input(21)
+    x=GPIO.input(21)
         if (x==0): 
             print "Just Awesome" 
             client.publish("Device/data", payload="To the World NCT!!" , qos=0, retain=False)
