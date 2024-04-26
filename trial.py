ca3c195d72f041816b920f31ab4b73b88ce71c50
Czennie <83468952+PatienceNyasulu@users.noreply.github.com>
2024-04-26 10:22:34 +0200

    Add files via upload

---
+++ trial.py
@@ -0,0 +1,23 @@
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+import time
+
+def helloworld(self, params, packet):
+ print ('Recieved Message from AWS IoT Core')
+ print ('Topic: '+ packet.topic)
+ print ("Payload: ", (packet.payload))
+
+
+myMQTTClient = AWSIoTMQTTClient ("raspberry") #random key, if anotier connection using the same key is
+myMQTTClient.configureEndpoint("a3s1cqgu0ggloe-ats.iot.eu-central-1.amazonaws.com", 8883)
+
+myMQTTClient. configureCredentials("AmazonRootCA1.pem", "ab41d0a6fc44dea8cbdaf000498a154a78feddc4e0a17270d035dbddb642b8b8-private.pem.key", "ab41d0a6fc44dea8cbdaf000498a154a78feddc4e0a17270d035dbddb642b8b8-certificate.pem")
+
+myMQTTClient.configureofflinePublishQueueing(-1) # Infinite offline Publish queueing 
+myMQTTClient.configureDrainingFrequency(2) # Draining: 2 HZ 
+myMQTTClient.configureConnectDisconnectTimeout (10) # 10 sec
+myMQTTClient.configureMQTTOperationTimeout (5) # 5 sec
+print ('Initiating IoT Core Topic ... ') 
+myMQTTClient.connect ( )
+myMQTTClient.subscribe("home/helloworld", 1, helloworld)
+                        
+                
