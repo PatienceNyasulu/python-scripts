9b6ff67f79ff228e4439e52eabadab1d4d4392fb
Czennie <83468952+PatienceNyasulu@users.noreply.github.com>
2024-04-26 10:26:55 +0200

    Update trial.py

--- trial.py
+++ trial.py
@@ -10,7 +10,7 @@ def helloworld(self, params, packet):
 myMQTTClient = AWSIoTMQTTClient ("raspberry") #random key, if anotier connection using the same key is
 myMQTTClient.configureEndpoint("a3s1cqgu0ggloe-ats.iot.eu-central-1.amazonaws.com", 8883)
 
-myMQTTClient. configureCredentials("AmazonRootCA1.pem", "ab41d0a6fc44dea8cbdaf000498a154a78feddc4e0a17270d035dbddb642b8b8-private.pem.key", "ab41d0a6fc44dea8cbdaf000498a154a78feddc4e0a17270d035dbddb642b8b8-certificate.pem")
+myMQTTClient. configureCredentials("AmazonRootCA1.pem", "ab41d0a6fc44dea8cbdaf000498a154a78feddc4e0a17270d035dbddb642b8b8-private.pem.key", "ab41d0a6fc44dea8cbdaf000498a154a78feddc4e0a17270d035dbddb642b8b8-certificate.pem.crt")
 
 myMQTTClient.configureofflinePublishQueueing(-1) # Infinite offline Publish queueing 
 myMQTTClient.configureDrainingFrequency(2) # Draining: 2 HZ 
